ba027e37e51e2a9a1a09f7e691792937d807048e
pesanth <pesanth10@gmail.com>
2026-07-23 14:35:34 -0300

    Document historical status and remove stale endpoints

--- react_frontend/src/api/ApiBaseURL.jsx
+++ react_frontend/src/api/ApiBaseURL.jsx
@@ -2,13 +2,6 @@ import axios from "axios";
 
 export const apiClient = axios.create(
   {
-  //for local machine
-  //baseURL: 'http://localhost:8080'
-
-  //for GCP vm instance
-  //baseURL: 'http://34.148.248.82:8080'
-
-  //for Azure vm instance
-  baseURL: 'http://40.76.224.112:8080'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080'
   }
 )
